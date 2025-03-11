a499d3e6d9b9a32623e306022e330c8545bb53c2
DSM3 <fatec-dsm3@academico.intra>
2025-03-11 20:03:54 -0300

    @LucasMiura

--- DW3/api-the-game/routes/gameRoutes.js
+++ DW3/api-the-game/routes/gameRoutes.js
@@ -3,6 +3,12 @@ const gameRoutes = express.Router();
 import gameController from "../controllers/gameController.js";
 
 // Endpoint para listar todos os games (rota)
-gameRoutes.get("/games", gameController.getAllGames)
+gameRoutes.get("/games", gameController.getAllGames);
+
+// Endpoint para cadastrar um jogo
+gameRoutes.post("/games", gameController.createGame);
+
+// Endpont para excluir um jogo
+gameRoutes.delete("/games/:id", gameController.deleteGame);
 
 export default gameRoutes;
